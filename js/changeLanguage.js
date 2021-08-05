0a0100c6696645109f67884ccfeca5d991662d85
dobkir <dobkir@gmail.com>
2021-08-06 00:51:30 +0400

    changeLanguage.js has been refactoring. Now selected language would be simultaneously changed on all pages of the website. LocalStorage has been used for that effect.

--- js/changeLanguage.js
+++ js/changeLanguage.js
@@ -1,15 +1,27 @@
 import { translationArr } from "./translation.js";
 
-const selectLanguage = document.querySelector('.change-lang');
-const possibleLanguages = ['en', 'ru'];
+const selectedLanguage = document.querySelector(".change-lang");
+let currentLanguage = selectedLanguage.value;
 
-selectLanguage.addEventListener('change', changeURLLanguage);
+const possibleLanguages = ["en", "ru"];
 
-// redirect to url with language indication in hash
-function changeURLLanguage() {
-  let lang = selectLanguage.value;
-  location.href = `${window.location.pathname}#${lang}`;
+selectedLanguage.addEventListener("change", function (event) {
+  event.stopPropagation();
+  changeURLLanguage();
   location.reload();
+});
+
+function changeURLLanguage() {
+  currentLanguage = selectedLanguage.value;
+
+  if (window.hasOwnProperty("localStorage"))
+    window.localStorage.setItem("lang", currentLanguage);
+  location.href = `${window.location.pathname}#${currentLanguage}`;
+}
+
+if ((window.hasOwnProperty("localStorage")) && (window.localStorage.getItem("lang") !== currentLanguage)) {
+  selectedLanguage.value = window.localStorage.getItem("lang");
+  changeURLLanguage();
 }
 
 function changeLanguage() {
@@ -19,13 +31,12 @@ function changeLanguage() {
   // If the pathname has an error, then use a default pathname: (#ru)
   // The best practice, in this case, is to use the geo-position of a user browser.
   if (!possibleLanguages.includes(hash)) {
-    location.href = window.location.pathname + '#ru';
+    location.href = window.location.pathname + "#ru";
+    currentLanguage = hash;
     location.reload();
   }
 
-  selectLanguage.value = hash;
-
-  const translationDOMElems = document.querySelectorAll('[data-translate]');
+  const translationDOMElems = document.querySelectorAll("[data-translate]");
 
   translationDOMElems.forEach(elem => {
     for (let key in translationArr) {
